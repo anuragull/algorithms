cbb661295e10894acde82468afe56a249329566b
anuragull <anurag.ull@gmail.com>
2018-05-03 23:59:02 -0400

    close matches using diff lib

--- name_matching/name_match.py
+++ name_matching/name_match.py
@@ -1,5 +1,7 @@
 import argparse
 from metaphone import doublemetaphone
+import difflib
+import heapq
 
 male_name = 'data/male.txt'
 female_name = 'data/female.txt'
@@ -8,7 +10,7 @@ all_words = 'data/english_word_ntk.txt'
 class NameMatch(object):
     def __init__(self):
         self.all_names = set()
-        self.metaphone_set = set()
+        self.metaphone_dict = {}
         self.set_name_list()
 
     def set_name_list(self):
@@ -19,7 +21,7 @@ class NameMatch(object):
 
         for names in self.all_names:
             first, second = doublemetaphone(names)
-            self.metaphone_set.add(first + second)
+            self.metaphone_dict[first + second] = names
 
     def direct_match(self, name):
         if name in self.all_names:
@@ -27,14 +29,17 @@ class NameMatch(object):
         return False
 
     def edit_dist(self, name):
+	# create a heap with the distances for the word
         pass
 
     def metaphone_match(self, name):
         test_f, test_s = doublemetaphone(name)
         test_metaphone = test_f+test_s
-        if test_metaphone in self.metaphone_set:
-            return True
-        return False
+        if test_metaphone in self.metaphone_dict:
+	    return self.metaphone_dict[test_metaphone]
+        return "None"
+    def close_match(self, name):
+        return difflib.get_close_matches(name, list(self.all_names), 10)
 
 def driver():
     argp = argparse.ArgumentParser(description="Name matching tool")
@@ -46,7 +51,9 @@ def driver():
     if args.Mode == 1:
         print("Name %s matches the database %r" % (args.Name, name_match.direct_match(args.Name)))
     if args.Mode == 2:
-        print("Name %s matches the metaphone database %r" % (args.Name, name_match.metaphone_match(args.Name)))
+        print("Name %s matches the metaphone database %s" % (args.Name, name_match.metaphone_match(args.Name)))
+    if args.Mode == 3:
+        print("Name %s matches list %s" % (args.Name, str(name_match.close_match(args.Name))))
 
 if __name__ == '__main__':
     driver()
