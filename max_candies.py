94c8c28a7c414786f0f61eec5dbd99b52a0410ae
Anurag Singh <anurag.ull@gmail.com>
2021-07-03 23:17:09 -0700

    Create max_candies.py

---
+++ max_candies.py
@@ -0,0 +1,62 @@
+import math
+# Add any extra import statements you may need here
+import heapq
+
+# Add any helper functions you may need here
+
+
+def maxCandies(arr, k):
+  # Write your code here
+  # create a max heap, add to it
+  heap = [ -1 * x for x in arr]
+  heapq.heapify(heap)
+  
+  running_sum = 0
+  while(k > 0):
+      # get the max element 
+      element =  -1 * heapq.heappop(heap)
+      running_sum += element
+      heapq.heappush(heap, -1 * math.floor(element/2))
+      k -= 1
+  return running_sum
+
+# These are the tests we use to determine if the solution is correct.
+# You can add your own at the bottom, but they are otherwise not editable!
+
+def printInteger(n):
+  print('[', n, ']', sep='', end='')
+
+test_case_number = 1
+
+def check(expected, output):
+  global test_case_number
+  result = False
+  if expected == output:
+    result = True
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if result:
+    print(rightTick, 'Test #', test_case_number, sep='')
+  else:
+    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
+    printInteger(expected)
+    print(' Your output: ', end='')
+    printInteger(output)
+    print()
+  test_case_number += 1
+
+if __name__ == "__main__":
+  n_1, k_1 = 5, 3
+  arr_1 = [2, 1, 7, 4, 2]
+  expected_1 = 14
+  output_1 = maxCandies(arr_1, k_1)
+  check(expected_1, output_1)
+
+  n_2, k_2 = 9, 3
+  arr_2 = [19, 78, 76, 72, 48, 8, 24, 74, 29]
+  expected_2 = 228
+  output_2 = maxCandies(arr_2, k_2)
+  check(expected_2, output_2)
+
+  # Add your own test cases here
+  
