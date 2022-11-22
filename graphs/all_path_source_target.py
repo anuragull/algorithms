894b4562b3aaddf5eef393811a88c85e35c6717b
Anurag Singh <anurag.ull@gmail.com>
2022-11-22 12:51:42 -0800

    Create all_path_source_target.py

---
+++ graphs/all_path_source_target.py
@@ -0,0 +1,30 @@
+  """
+  BFS:
+  queue 
+  path list
+  temp_path = copy of current_path, append node
+  if we saw all the paths - append to the list else add to queue 
+  """
+  def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        
+        
+        paths = []
+        if not graph or len(graph) == 0:
+            return paths
+
+        queue = deque()
+        path = [0]
+        queue.append(path)
+
+        while queue:
+            current_path = queue.popleft()
+            node = current_path[-1]
+            for next_node in graph[node]:
+                temp_path = current_path.copy()
+                temp_path.append(next_node)
+
+                if next_node == len(graph) - 1:
+                    paths.append(temp_path)
+                else:
+                    queue.append(temp_path)
+        return paths
